753c95947040d4b86070f6b38f6ba07d1f8e8ae8
unknown <mjrjuyel8@gmail.com>
2023-08-30 21:02:36 +0600

    Experinece , Video, Sub ,Place , sponsor , Footer section done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -14,6 +14,8 @@ const navMenu = document.getElementById('nav-menu'),
           navMenu.classList.remove('show-menu')
         })
       }
+
+// header back gound
 function scrollHeader(){
   const Header= document.getElementById('header')
 
@@ -23,12 +25,13 @@ function scrollHeader(){
   Header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
-      
+
+
+  // swiper container
     var swiper = new Swiper(".swiper-container", {
       effect: "coverflow",
       grabCursor: true,
       loop: true,
-      zoom: true,
       centeredSlides: true,
       slidesPerView: "auto",
       coverflowEffect: {
@@ -39,4 +42,45 @@ window.addEventListener('scroll', scrollHeader)
         clickable: true,
       },
     })
- 
+
+    // video play
+    const VideoPlay = document.getElementById('video_id') ,
+          videoButton = document.getElementById('video_button') ,
+          videoIcon = document.getElementById('video_icon')
+
+          function playPause(){
+            if(VideoPlay.paused)
+            {
+                VideoPlay.play();
+                videoIcon.classList.remove('ri-play-line')
+                videoIcon.classList.add('ri-pause-line')
+            }
+
+            else
+                {
+                VideoPlay.pause()
+                videoIcon.classList.remove('ri-pause-line')
+                videoIcon.classList.add('ri-play-line')
+                }
+          }
+        videoButton.addEventListener('click', playPause)
+
+        function videoEnd(){
+          videoIcon.classList.remove('ri-pause-line')
+          videoIcon.classList.add('ri-play-line')
+        }
+        VideoPlay.addEventListener('ended', videoEnd)
+
+        // ScrollUp Buttom
+ function scrollUpButton(){
+  const scrollUp= document.getElementById('scroll_up')
+
+  if(this.scrollY >= 300){
+    scrollUp.classList.add('up_button');
+  }
+  else{
+    scrollUp.classList.remove('up_button');
+  }
+ }
+
+ window.addEventListener('scroll', scrollUpButton);
